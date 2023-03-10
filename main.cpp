d6a8ca2ea8b14a7c3c7d86db60f7730cfcb37a29
Francesco Andreotti <francesco.andreotti@mail.polimi.it>
2023-03-10 12:20:28 +0000

    modified main.cpp with greating from Fra

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #che succede se modifico senza creare un branch?
 int main() {
-	std::cout << Greetings from Giulia! <<  std::endl;
+	std::cout << Greetings from Francesco! <<  std::endl;
 	return 0;
 }
 
